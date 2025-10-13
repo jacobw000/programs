3d2967491877531d1ada4eab209506e4afab17d3
jacobw000 <jacobwinwood@icloud.com>
2025-10-13 09:32:00 +0100

    Add files via upload

---
+++ interestCalc.py
@@ -0,0 +1,6 @@
+def interest(p,r,t):
+    p = float(input("Enter current balance in account."))
+    r = float(input("Enter interest rate as a percentage."))
+    t = float(input("Enter the number of years the money will gain interest on."))
+    total =  p * r * t
+
---
+++ interestTest.py
@@ -0,0 +1,8 @@
+import interestCalc
+p = float(input("Enter current balance in account."))
+r = float(input("Enter interest rate as a percentage."))
+t = float(input("Enter the number of years the money will gain interest on."))
+
+
+print(interestCalc.interest(p,r,t))
+
